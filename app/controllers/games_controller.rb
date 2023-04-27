04a836fd83d17caf42dc19a4c808698621b87a54
Vittoria Sajeva vittoria.sajeva@gmail.com <vittoria.sajeva@gmail.com>
2023-04-27 21:47:48 +0100

    rails-longest-word-game completed

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,25 @@
+require "open-uri"
+
+class GamesController < ApplicationController
+  def new
+    @letters = ('A'..'Z').to_a.sample(10)
+    @start_time = Time.now
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || '').upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
